7c0b1d400c1124fb36a30a6bb30d90ad813169c9
Dump Dev <dump.dev.bruno@gmail.com>
2025-08-23 18:23:09 -0400

    test(e2e): add test where credentials not match

--- src/tests/auth.e2e.test.ts
+++ src/tests/auth.e2e.test.ts
@@ -19,7 +19,7 @@ describe("Router /auth", () => {
   });
 
   describe("POST /auth/login", () => {
-    describe("when user sending valid crendentails", () => {
+    describe("when user sends valid credentials", () => {
       describe("and credentials match", () => {
         test("should return 200 and an accessToken", async () => {
           const testAgent = request(app);
@@ -33,6 +33,25 @@ describe("Router /auth", () => {
           const response = await loginUser(testAgent, user);
           expect(response.statusCode).toBe(StatusCodes.OK);
           expect(response.body).toHaveProperty("accessToken");
+        });
+      });
+
+      describe("and credentials do not match", () => {
+        test("should return 401 and have error message", async () => {
+          const testAgent = request(app);
+          const user = {
+            name: "John Doe",
+            email: "johndoe@test.com",
+            password: "super secure password",
+          };
+          await registerUser(testAgent, user);
+          const credentials = {
+            email: "johndoe@test.com",
+            password: "another pass",
+          };
+          const response = await loginUser(testAgent, credentials);
+          expect(response.statusCode).toBe(StatusCodes.UNAUTHORIZED);
+          expect(response.body).toHaveProperty("message");
         });
       });
     });
