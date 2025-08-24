61c9aa391a2d535fb0ba174f10b3896b372274fd
Dump Dev <dump.dev.bruno@gmail.com>
2025-08-24 19:17:47 -0400

    refactor: remove the old seed format

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -3,14 +3,12 @@ import "dotenv/config";
 import express from "express";
 import "reflect-metadata";
 import errorHandlerMiddleware from "./middlewares/error-handler.middleware";
+import { connectRedis } from "./redis-client/client";
 import router from "./router";
 import { connectDB } from "./typeorm/data-source";
-import seedRolesAndPermissions from "./typeorm/roles-and-permissions.seed";
-import { connectRedis, closeConnectionRedis } from "./redis-client/client";
 
 export default async function bootstrap() {
   await Promise.all([connectDB(), connectRedis()]);
-  if (process.env.NODE_ENV !== "production") await seedRolesAndPermissions();
   const app = express();
   app.use(cors());
   app.use(express.json());
